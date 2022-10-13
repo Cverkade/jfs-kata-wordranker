409d2cff37e5b432fe8a757737023498cdbfe740
jelmstrom <johan.elmstrom@gmail.com>
2022-10-13 10:49:13 +0200

    created kata

---
+++ src/main/java/se/salt/jfs/kta/wordranker/WordRanker.java
@@ -0,0 +1,9 @@
+package se.salt.jfs.kta.wordranker;
+
+public class WordRanker {
+
+
+    public String highestRanking(String sentence){
+        return "";
+    }
+}
---
+++ src/test/java/se/salt/jfs/kta/wordranker/WordRankerTest.java
@@ -0,0 +1,16 @@
+package se.salt.jfs.kta.wordranker;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordRankerTest {
+
+
+    WordRanker ranker = new WordRanker();
+
+    @Test
+    void shouldRateBrownHighest() {
+        String word = ranker.highestRanking("The quick brown fox");
+        assertEquals("brown", word);
+    }
+}
