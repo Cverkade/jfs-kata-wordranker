b8eed63e86c417f44d2e3ae2688f90af3ef18822
Max Burrows <maxajburrows@gmail.com>
2023-06-01 19:53:11 +0200

    Revert "Passes existing test"
    This reverts commit 3e6f740008670007b7dad3d04ffa51ea63bcb61e.

--- src/main/java/se/salt/jfs/kta/wordranker/WordRanker.java
+++ src/main/java/se/salt/jfs/kta/wordranker/WordRanker.java
@@ -1,20 +1,9 @@
 package se.salt.jfs.kta.wordranker;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class WordRanker {
-    public String highestRanking(String sentence) {
-        if (sentence == " ") {
-            throw new IllegalArgumentException("Sentence must not be empty");
-        }
-        String[] splitSentence = sentence.split(" ");
-        List<Integer> wordScores = Arrays.stream(splitSentence)
-                .map(str -> str.replaceAll("(?i)[^a-z]", ""))
-                .map(str -> (int) str.chars().sum())
-                .toList();
-        int maxIndex = wordScores.indexOf(Collections.max(wordScores));
-        return splitSentence[maxIndex].replaceAll("(?i)[^a-z]", "");
+
+
+    public String highestRanking(String sentence){
+        return "";
     }
 }
