d903a2575ec6ca6fa9b594ecb6ed127f1df2bad2
Cornelis Verkade <cornelisverkade@Corneliss-MacBook-Air.local>
2023-06-01 20:58:27 +0200

    working solution

--- src/main/java/se/salt/jfs/kta/wordranker/WordRanker.java
+++ src/main/java/se/salt/jfs/kta/wordranker/WordRanker.java
@@ -1,9 +1,33 @@
 package se.salt.jfs.kta.wordranker;
 
+import java.sql.Array;
+
 public class WordRanker {
 
-
     public String highestRanking(String sentence){
-        return "";
+
+        String[] words = sentence.split(" ");
+
+        if (words.length == 0 || words[0] == ""){
+            throw new IllegalArgumentException("Please enter a word");
+        }
+
+        int highestValue = 0;
+        String highestRankingWord = "";
+
+        for (String word:words) {
+            int wordValue = 0;
+
+            for (int i = 0; i < word.length(); i++) {
+                if (Character.isAlphabetic(word.charAt(i))){
+                    wordValue += word.charAt(i);
+                }
+                if (wordValue > highestValue){
+                    highestValue = wordValue;
+                    highestRankingWord = word;
+                }
+            }
+        }
+        return highestRankingWord;
     }
 }
